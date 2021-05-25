dfdd931d1e81972f6a52d018a228dc859e0f245d
yanglw <yangliangwei@quzeng.com>
2021-05-25 11:01:10 +0800

    first commit

---
+++ app/src/main/java/me/yanglw/android/tv/placeholder/MainActivity.java
@@ -0,0 +1,22 @@
+package me.yanglw.android.tv.placeholder;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            Intent intent = getPackageManager().getLaunchIntentForPackage(BuildConfig.TARGET_PACKAGE_NAME);
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        finish();
+    }
+}
